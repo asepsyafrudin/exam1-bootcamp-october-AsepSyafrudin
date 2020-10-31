bcc761171176e4279adc5d338001dc00b6a1ea35
asepsyafrudin <asseeppsyafrudin@gmail.com>
2020-10-31 22:50:23 +0700

    merapikan sedikit

--- src/co/g2academy/VectorOperation.java
+++ src/co/g2academy/VectorOperation.java
@@ -6,6 +6,7 @@ public class VectorOperation {
         int lengthVector1 = vector1.length;
         int lengthVector2 = vector2.length;
         int[] result = null;
+
         if(lengthVector1 !=0 && lengthVector2!=0) {
             if(lengthVector1 == lengthVector2 ) {
                 result = new int[lengthVector1];
@@ -23,16 +24,14 @@ public class VectorOperation {
         int lengthVector1 = vector1.length;
         int lengthVector2 = vector2.length;
         int [] result = null;
+
         if(lengthVector1 !=0 && lengthVector2!=0){
             if(lengthVector1 == lengthVector2){
                 result = new int [vector1.length];
                 for(int i = 0 ; i<lengthVector1 ;  i++) {
                     result[i] = vector1[i]*vector2[i];
                 }
-            } else {
-                return null;
             }
-
         }
 
         return  result;
